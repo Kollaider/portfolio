cefd8a2033fdc3ee4af32431b09338d5f9b7b867
Kollaider <kolja87@tut.by>
2022-10-12 19:22:29 +0300

    adde search_result.html

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from webapp.views import index, about
+from webapp.views import index, about, search
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
+    path('search/', search, name='search_results')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 
 from webapp.models import Project
@@ -11,3 +12,14 @@ def index(request):
 def about(request):
 
     return render(request, 'webapp/about.html')
+
+
+def search(request):
+    text = request.GET.get('search')
+    if text:
+        projects = Project.objects.filter(
+            Q(title__contains=text)|Q(description__contains=text)
+        )
+    else:
+        projects = Project.objects.none()
+    return render(request, 'webapp/search_results.html', {'projects': projects})
