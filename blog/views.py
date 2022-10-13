721da852459ffabbf524e1340cd27bc56f77400c
Kollaider <kolja87@tut.by>
2022-10-13 16:18:14 +0300

    added CommentForm

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormMixin
 
 from blog.models import Post, Category, Comment
+from blog.form import CommentForm
 
 
 # def blog_index(request):
@@ -18,16 +21,31 @@ class LBlogView(ListView):
         return posts
 
 
-# def blog_detail(request, pk):
-#     post = Post.objects.get(pk=pk)
-#     comments = Comment.objects.filter(post=post)
-#     context = {'post': post, 'comments': comments}
-#     return render(request, 'blog/blog_detail.html', context=context)
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data['author'],
+                body=form.cleaned_data['body'],
+                post=post
+            )
+            comment.save()
+
+    comments = Comment.objects.filter(post=post)
+    context = {
+        'post': post,
+        'comments': comments,
+        'form': form,
+    }
+    return render(request, 'blog/blog_detail.html', context=context)
 
 
-class DetailBlogView(DetailView):
-    model = Post
-    template_name = 'blog/blog_detail.html'
+
+
+
 
 
 def blog_category(request, category):
